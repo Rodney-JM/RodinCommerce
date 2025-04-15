8cb19ac6704a344755831c651c374ddaddf424d1
Rodney <rondineypatricio@gmail.com>
2025-04-15 18:23:41 -0300

    migration studies

--- database/migrations/2025_04_15_205235_excluir_table_produto.php
+++ database/migrations/2025_04_15_205235_excluir_table_produto.php
@@ -4,17 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProdutosTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::dropIfExists('produto');
     }
 
     /**
@@ -22,6 +19,6 @@ class CreateProdutosTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('produtos');
+        //
     }
 };
